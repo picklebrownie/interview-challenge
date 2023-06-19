35c00886a578d1e764fcd41f0a91435c6294f8f6
Kayla <this.browniedev@gmail.com>
2023-06-19 15:28:03 -0400

    Fix: Remove short tags

--- src/reset-password.php
+++ src/reset-password.php
@@ -23,7 +23,7 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
     if (empty(trim($_POST["new_password"]))) {
         $new_password_err = "Please enter the new password.";
     } elseif (strlen(trim($_POST["new_password"])) < 6) {
-        $new_password_err = "Password must have atleast 6 characters.";
+        $new_password_err = "Password must have at least 6 characters.";
     } else {
         $new_password = trim($_POST["new_password"]);
     }
